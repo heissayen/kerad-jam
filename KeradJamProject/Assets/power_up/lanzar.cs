2ae85d7cce30aeec95520e76870b4f0c1be0901d
franciscofernandez <franciscofernandezmanyas@gmail.com>
2017-01-13 07:44:06 +0100

    added power up and fixes

---
+++ KeradJamProject/Assets/power_up/lanzar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lanzar : MonoBehaviour {
+
+	public GameObject punta;
+	public GameObject audio_contr;
+	private Rigidbody rb;
+	private float fuerza = 50.0f;
+
+	void OnTriggerEnter(Collider coll){
+		//coll.gameObject.GetComponent<Rigidbody> ()
+		Vector3 dir = punta.transform.position - transform.position;
+		rb = coll.gameObject.GetComponent<Rigidbody> ();
+		rb.velocity = Vector3.zero;
+		rb.velocity = dir.normalized * fuerza;
+		audio_contr.GetComponent<AudioManager> ().playSound ("power_up");
+		Destroy (gameObject);
+	}
+}
