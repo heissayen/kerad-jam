588898bf2110b1d697fa496b9dc7f5cc1e3d7cc9
franciscofernandez <franciscofernandezmanyas@gmail.com>
2017-01-13 05:17:18 +0100

    added fixed canvas and sound

--- KeradJamProject/Assets/Canvas/canvas.cs
+++ KeradJamProject/Assets/Canvas/canvas.cs
@@ -5,18 +5,25 @@ using UnityEngine.UI;
 public class canvas : MonoBehaviour {
 
 	private float start_time;
-	public float loading_time = 3.0f;
+	public float loading_time = 1.5f;
 
 	public GameObject pantalla_carga;
 	public GameObject pantalla_menu;
 	public GameObject pantalla_busqueda;
-	public GameObject cabecera_menu;
 	public GameObject cabecera_puntuacion;
 
 	public Sprite[] skins;
 	public Sprite[] stats;
+	public Sprite[] icons;
 	public GameObject jugador;
 	public GameObject barras;
+	public GameObject icono;
+	public GameObject nubes;
+	private float  timeStartedLerping;
+	public float LerpTime = 300.0f;
+	private bool isLerping;
+	private Vector3 startPosition;
+	private Vector3 endPosition;
 	private int puntero = 0;
 
 	// Use this for initialization
@@ -24,40 +31,68 @@ public class canvas : MonoBehaviour {
 		pantalla_carga.SetActive (true);
 		pantalla_menu.SetActive(false);
 		pantalla_busqueda.SetActive(false);
-		cabecera_menu.SetActive (false);
 		cabecera_puntuacion.SetActive (false);
 		start_time = Time.time;
 		jugador.GetComponent<Image> ().sprite = skins [puntero];
 		barras.GetComponent<Image> ().sprite = stats [puntero];
+		icono.GetComponent<Image> ().sprite = icons [puntero];
+		timeStartedLerping = Time.time;
+		startPosition = nubes.transform.position;
+		endPosition = new Vector3 (-startPosition.x, startPosition.y, 0.0f);
+		isLerping = true;
 	}
 
 	void Update(){
 		if (Time.time > start_time + loading_time) {
 			pantalla_carga.SetActive (false);
 			pantalla_menu.SetActive(true);
-			cabecera_menu.SetActive (true);
+		}
+
+		if (isLerping) {
+			float timeSinceStarted = Time.time - timeStartedLerping;
+			float percentageComplete = timeSinceStarted / LerpTime;
+			nubes.transform.position = Vector3.Lerp (startPosition, endPosition, percentageComplete);
+			if (percentageComplete >= 0.95f) {
+				isLerping = false;
+				nubes.transform.position = endPosition;
+			}
 		}
 	}
-
+	//Display de menu de busqueda llamado desde menu principal
 	public void battle(){
 		pantalla_busqueda.SetActive (true);
-		cabecera_menu.SetActive (false);
 		//CODIGO DE BUSQUEDA DE RIVAL
 	}
-
+	//Display de menu principal llamado desde menu de busqueda
 	public void back(){
-		cabecera_menu.SetActive (true);
+		pantalla_menu.SetActive(true);
 		pantalla_busqueda.SetActive (false);
 		//CODIGO CANCELAR BUSQUEDA DEL RIVAL
 	}
-
-	public void next(){
-		jugador.GetComponent<Image> ().sprite = skins [(puntero+1)%skins.Length];
-		barras.GetComponent<Image> ().sprite = stats [(puntero+1)%skins.Length];
+	//Display dentro de partida llamado desde menu de busqueda
+	public void match(){
+		pantalla_busqueda.SetActive (false);
+		cabecera_puntuacion.SetActive (true);
+	}
+	//Display de menu principal desde menu de partida
+	public void menu(){
+		pantalla_menu.SetActive(true);
+		cabecera_puntuacion.SetActive (false);
 	}
 
+	//Funcion para cambiar el pj desde menu principal
+	public void next(){
+		++puntero;
+		jugador.GetComponent<Image> ().sprite = skins [Mathf.Abs((puntero)%skins.Length)];
+		barras.GetComponent<Image> ().sprite = stats [Mathf.Abs((puntero)%skins.Length)];
+		icono.GetComponent<Image> ().sprite = icons [Mathf.Abs((puntero)%skins.Length)];
+	}
+
+	//Funcion para cambiar el pj desde menu principal
 	public void previous(){
-		jugador.GetComponent<Image> ().sprite = skins [(puntero-1)%skins.Length];
-		barras.GetComponent<Image> ().sprite = stats [(puntero-1)%skins.Length];
+		--puntero;
+		jugador.GetComponent<Image> ().sprite = skins [Mathf.Abs((puntero)%skins.Length)];
+		barras.GetComponent<Image> ().sprite = stats [Mathf.Abs((puntero)%skins.Length)];
+		icono.GetComponent<Image> ().sprite = icons [Mathf.Abs((puntero)%skins.Length)];
 	}
 }
---
+++ KeradJamProject/Assets/Sound/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+
+	public GameObject auxiliar;		//objeto para variables temporales
+	public static AudioManager instance = null;
+
+	public AudioClip[] sounds;
+	public string[] names;
+
+	/*
+	void Start(){
+		setAudioManager ("background_sound");
+	}
+	*/
+	//Reproduccion simple, puede haber multiples
+	public void playSound(string name){
+		if (System.Array.IndexOf(names,name)>=0) {
+			GameObject inst = Instantiate (auxiliar);
+			AudioClip temp = sounds[System.Array.IndexOf(names,name)];
+			inst.GetComponent<Audio> ().PlaySoundOnce (temp);
+		}
+	}
+	//Sonido en loop, solo puede haber 1
+	public void setAudioManager(string bgs_name){
+		//Comprobamos que no haya otra instancia de AudioManager
+		if (instance == null) instance = this;
+		else 
+			if (instance != this) 
+				Destroy (gameObject);
+		DontDestroyOnLoad (gameObject);
+
+		//Repdroducimos el sonido de fondo
+		AudioSource aus = GetComponent<AudioSource> ();
+		aus.clip = sounds[System.Array.IndexOf(names,bgs_name)];
+		aus.loop = true;
+		aus.Play();
+		Debug.Log (aus.clip.name);
+	}
+}
